d12cf1326c6910826571807b759b4341a6371478
vinesk <vinesk.dev@gmail.com>
2024-03-26 14:46:37 +0100

    First version working

--- src/header/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css'
-
-export default function header() {
-  const header = document.createElement('header')
-  header.classList.add('header')
-  header.textContent = 'Header'
-
-  return header
-}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import './index.css'
-const { default: header } = require('./header')
 
-const app = document.querySelector('#app')
-app.appendChild(header())
+document.getElementById('search-btn').addEventListener('click', function () {
+  const city = document.getElementById('search-input').value.trim()
+  fetchWeatherData(city)
+})
+
+async function fetchWeatherData(city) {
+  const apiKey = '83f3df5664574d36931184246242503'
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=fr`
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    displayWeatherData(data)
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données météo:', error)
+  }
+}
+
+function displayWeatherData(data) {
+  const weatherElement = document.getElementById('weather-result')
+  if (data.error) {
+    weatherElement.innerHTML = `<p>${data.error.message}</p>`
+  } else {
+    weatherElement.innerHTML = `
+      <h2>${data.location.name}, ${data.location.country}</h2>
+      <img src="https:${data.current.condition.icon}" alt="Weather Icon" />
+      <p>Température: ${data.current.temp_c}°C / ${data.current.temp_f} °F</p>
+      <p>Condition: ${data.current.condition.text}</p>
+      <p>Humidité: ${data.current.humidity}%</p>`
+  }
+}
